4af3c9eadb86670265055605fe406543df425040
joan.garcia <joan.garcia@ceiba.com.co>
2019-06-18 08:58:48 -0500

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { UserComponent } from './user/user.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path :'', redirectTo: '/userComponent', pathMatch: 'full'},
+  {path: 'appComponent', component: AppComponent},
+  {path: 'userComponent', component: UserComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
