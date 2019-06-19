511a56faceaa0cb6a82842d57620c01645bbb338
joan.garcia <joan.garcia@ceiba.com.co>
2019-06-18 23:47:38 -0500

    Payment message

--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -45,8 +45,8 @@ export class VehicleComponent implements OnInit {
         this.payment = res;
         //this.vehicleService.getVehicles();
        //this.router.navigate(['/vehicleComponent']);
-       //this.toast.success("Su "+res['typeVehicle']+" de placa "+ res['plate'],"REGISTRO DE INGRESO");
-       location.reload();
+       this.toast.success("$ "+this.payment,"VALOR A PAGAR");
+       //location.reload();
       }
     }, err => {
       this.toast.error(err,"Error al listar vehiculos");
